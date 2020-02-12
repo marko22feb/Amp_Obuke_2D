394412291b8566269dfb46c75694a90ba79f8e92
RoiaLaptop <45495716+UnderdogTagTeam@users.noreply.github.com>
2020-02-12 20:32:59 +0100

    Prvi Cas

---
+++ Assets/MovementComponent.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementComponent : MonoBehaviour
+{
+
+    private Transform tr;
+
+    void Start()
+    {
+       tr = GetComponent<Transform>();
+    }
+
+ 
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            Vector2 newposition = new Vector2(tr.position.x + 0.01f, tr.position.y);
+            tr.position = newposition;
+            tr.rotation = new Quaternion(0, 0, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            Vector2 newposition = new Vector2(tr.position.x - 0.01f, tr.position.y);
+            tr.position = newposition;
+            tr.rotation = new Quaternion(0, 180, 0, 0);
+        }
+    }
+}
