c936f178c6966f4072d60071c36bc0071609bf25
RoiaLaptop <45495716+UnderdogTagTeam@users.noreply.github.com>
2020-02-16 22:17:57 +0100

    Drugi Cas

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private Transform tr;
+    public Transform PlayerTr;
+  
+
+    private void Start()
+    {
+        tr = GetComponent<Transform>();
+    }
+    void Update()
+    {
+        tr.position = new Vector3(PlayerTr.position.x, PlayerTr.position.y, -10);
+        
+    }
+}
--- Assets/MovementComponent.cs
+++ Assets/MovementComponent.cs
@@ -6,27 +6,72 @@ public class MovementComponent : MonoBehaviour
 {
 
     private Transform tr;
+    private Rigidbody2D rigidbody2d;
+    private Animator anim;
+    private RaycastHit2D raycasthit2D;
+    private BoxCollider2D boxcollider2D;
+    [SerializeField] private LayerMask floorLayerMask;
+
+    public float Speed = 5;
+    public float JumpHeight = 500;
 
     void Start()
     {
-       tr = GetComponent<Transform>();
+        tr = GetComponent<Transform>();
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        boxcollider2D = GetComponent<BoxCollider2D>();
     }
 
- 
+    private void Update()
+    {
+        anim.SetBool("IsOnGround", IsOnGround());
+
+        if (Input.GetAxis("Jump") > 0)
+        {
+            Jump();
+        }
+    }
+
     void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        if (IsOnGround())
         {
-            Vector2 newposition = new Vector2(tr.position.x + 0.01f, tr.position.y);
-            tr.position = newposition;
-            tr.rotation = new Quaternion(0, 0, 0, 0);
+            if (Input.GetAxis("Horizontal") > 0)
+            {
+                rigidbody2d.velocity = new Vector2(Input.GetAxis("Horizontal") * Speed, rigidbody2d.velocity.y);
+                tr.rotation = new Quaternion(0, 0, 0, 0);
+                anim.SetBool("IsRunning", true);
+            }
+
+            if (Input.GetAxis("Horizontal") < 0)
+            {
+                rigidbody2d.velocity = new Vector2(Input.GetAxis("Horizontal") * Speed, rigidbody2d.velocity.y);
+                tr.rotation = new Quaternion(0, 180, 0, 0);
+                anim.SetBool("IsRunning", true);
+            }
         }
 
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetAxis("Horizontal") == 0)
         {
-            Vector2 newposition = new Vector2(tr.position.x - 0.01f, tr.position.y);
-            tr.position = newposition;
-            tr.rotation = new Quaternion(0, 180, 0, 0);
+            anim.SetBool("IsRunning", false);
+        }
+    }
+
+    private bool IsOnGround()
+    {
+        raycasthit2D = Physics2D.BoxCast(boxcollider2D.bounds.center, boxcollider2D.bounds.size, 0f, Vector2.down, 0.1f, floorLayerMask);
+        if (raycasthit2D.collider != null)
+        {
+            return true;
+        }
+        else return false;
+    }
+    private void Jump()
+    {
+        if (IsOnGround())
+        {
+            rigidbody2d.velocity = new Vector2(rigidbody2d.velocity.x, JumpHeight);
         }
     }
 }
