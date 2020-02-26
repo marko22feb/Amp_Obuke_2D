330b9d0fc4dacfb6ffe1c3e7343d34acdd93ea4f
RoiaLaptop <45495716+UnderdogTagTeam@users.noreply.github.com>
2020-02-26 20:33:11 +0100

    5ti cas.

---
+++ Assets/DamageComponent.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageComponent : MonoBehaviour
+{
+    StatComponent EnemyStats;
+
+    void Start()
+    {
+        EnemyStats = GetComponent<StatComponent>();   
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            StatComponent playerStat = collision.gameObject.GetComponent<StatComponent>();
+            if (collision.transform.position.y - 1f < transform.position.y)
+            {
+                playerStat.ModifyHealthBy(-EnemyStats.damage);
+            }
+            else
+            {
+                EnemyStats.ModifyHealthBy(-playerStat.damage);
+            }
+
+            playerStat.gameObject.GetComponent<Rigidbody2D>().AddForce(transform.up * 500);
+        }
+    }
+}
---
+++ Assets/StatComponent.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatComponent : MonoBehaviour
+{
+    public float minHP = 0;
+    public float maxHP = 100;
+    public float currentHP = 100;
+    public float damage = 50;
+
+    public bool CanBeDealtDamage = true;
+
+    void Start()
+    {
+        
+    }
+
+    public void ModifyHealthBy(float by)
+    {
+        currentHP += by;
+        if (currentHP <= 0) OnDeath();
+        if (currentHP > maxHP) currentHP = maxHP;
+
+        if (tag == "Player" & by < 0) {
+            GetComponent<Animator>().SetTrigger("IsHurt");
+        }
+
+        if (by < 0) { CanBeDealtDamage = false; StartCoroutine(CanBeDamagedAgain()); }
+    }
+
+    void OnDeath()
+    {
+        if (tag == "Player")
+        {
+            Time.timeScale = 0;
+        }
+        else
+        {
+            StartCoroutine(DestroySelf());
+        }
+    }
+
+    IEnumerator DestroySelf()
+    {
+        GetComponent<Animator>().SetTrigger("IsDead");
+        yield return new WaitForSeconds(0.5f);
+        Destroy(this.gameObject);
+
+        StopCoroutine(DestroySelf());
+    }
+
+    IEnumerator CanBeDamagedAgain()
+    {
+        yield return new WaitForSeconds(3.5f);
+        CanBeDealtDamage = true;
+
+        StopCoroutine(CanBeDamagedAgain());
+    }
+}
